cd5badb4362227da97457f2e20cfef66873295b1
Gocha <gochazautashvili2017@gmail.com>
2024-06-19 14:50:49 +0400

    loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="sticky top-0 left-0 right-0 w-full h-screen bg-primary/80 flex items-center justify-center text-white text-3xl font-semibold">
+      Loading...
+    </div>
+  );
+};
+
+export default loading;
